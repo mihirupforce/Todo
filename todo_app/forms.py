b751559367bd6bb2f30f86a364550d15a05a7bcc
mihirupforce <126077250+mihirupforce@users.noreply.github.com>
2023-02-28 14:43:17 +0530

    Update forms.py

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -2,15 +2,12 @@ from django import forms
 
 
 class DateSearch(forms.Form):
-    start_date = forms.DateField(widget=forms.DateInput(
-        attrs={'type': 'date'}), required=False)
-    end_date = forms.DateField(widget=forms.DateInput(
-        attrs={'type': 'date'}), required=False)
+    start_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=False)
+    end_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=False)
 
     def clean(self):
         cleaned_data = super().clean()
         start_date = cleaned_data.get("start_date")
         end_date = cleaned_data.get("end_date")
         if (start_date and end_date) and (start_date > end_date):
-            raise forms.ValidationError(
-                "Start date should be smaller than enddate")
+            raise forms.ValidationError("Start date should be smaller than end date")
